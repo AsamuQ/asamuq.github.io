d69bfc35577d40ab43d25adaeefd9dea4441a57e
asamuq <1625094153@qq.com>
2020-05-21 23:06:54 +0800

    Site updated: 2020-05-21 23:06:49

--- assets/autoload.js
+++ assets/autoload.js
@@ -13,22 +13,3 @@ try {
         }});
     }});
 } catch(err) { console.log("[Error] JQuery is not defined.") }
-<script>
-        document.querySelectorAll('.github-emoji')
-          .forEach(el => {
-            if (!el.dataset.src) { return; }
-            const img = document.createElement('img');
-            img.style = 'display:none !important;';
-            img.src = el.dataset.src;
-            img.addEventListener('error', () => {
-              img.remove();
-              el.style.color = 'inherit';
-              el.style.backgroundImage = 'none';
-              el.style.background = 'none';
-            });
-            img.addEventListener('load', () => {
-              img.remove();
-            });
-            document.body.appendChild(img);
-          });
-      </script>
